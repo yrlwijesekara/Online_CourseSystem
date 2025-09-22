7b095db879942e43fcfd92b067a8b181bdb06789
Hirunidiv <hirunidivyanjalee365@gmail.com>
2025-09-22 07:31:27 +0530

    created Admin Instructor Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,28 @@
 import './App.css'
-import { useState } from 'react';
+// import { useState } from 'react';
 import HomePage from './pages/HomePage';
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage';
 import CoursePage from './pages/CoursePage';
+import AdminInstructors from './pages/AdminInstructors';
 
 
 function App() {
-  // Use state to manage which page is displayed
-  const [currentPage, setCurrentPage] = useState('home'); // Options: 'home', 'course', 'signin', 'signup'
+  // // Use state to manage which page is displayed
+  // const [currentPage, setCurrentPage] = useState('home'); // Options: 'home', 'course', 'signin', 'signup'
   
-  // Function to navigate between pages
-  const navigateTo = (page) => {
-    setCurrentPage(page);
-  };
+  // // Function to navigate between pages
+  // const navigateTo = (page) => {
+  //   setCurrentPage(page);
+  // };
 
   return (
     <>
-    {currentPage === 'course' && <CoursePage navigateTo={navigateTo} />}
+    {/* {currentPage === 'course' && <CoursePage navigateTo={navigateTo} />}
     {currentPage === 'signin' && <SignInPage navigateTo={navigateTo} />}
     {currentPage === 'signup' && <SignUpPage navigateTo={navigateTo} />}
-    {currentPage === 'home' && <HomePage navigateTo={navigateTo} />}
+    {currentPage === 'home' && <HomePage navigateTo={navigateTo} />} */}
+    <AdminInstructors/>
     </>
   )
 } 
