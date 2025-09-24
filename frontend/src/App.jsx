1f5b95e016359a2920d25f98e240f8da58ca2233
Hirunidiv <hirunidivyanjalee365@gmail.com>
2025-09-24 19:00:59 +0530

    fixed App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,6 @@ import HomePage from './pages/HomePage';
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage';
 import CoursePage from './pages/CoursePage';
-
-
-function App() {
-  // // Use state to manage which page is displayed
-  const [currentPage, setCurrentPage] = useState('home'); // Options: 'home', 'course', 'signin', 'signup'
-
 import ContactUs from './pages/ContactUs';
 import AboutUs from './pages/AboutUs';
 import Admin from './pages/Admin/admin';
@@ -19,10 +13,9 @@ import AdminCourses from './pages/Admin/AdminCourses';
 function App() {
   // Use state to manage which page is displayed
   const [currentPage, setCurrentPage] = useState('signin'); // Start with signin page
-
   
-  // // Function to navigate between pages
-   const navigateTo = (page) => {
+  // Function to navigate between pages
+  const navigateTo = (page) => {
     setCurrentPage(page);
   };
 
@@ -31,7 +24,6 @@ function App() {
     {currentPage === 'course' && <CoursePage navigateTo={navigateTo} />}
     {currentPage === 'signin' && <SignInPage navigateTo={navigateTo} />}
     {currentPage === 'signup' && <SignUpPage navigateTo={navigateTo} />}
-    {currentPage === 'home' && <HomePage navigateTo={navigateTo} />}
     {currentPage === 'home' && <HomePage navigateTo={navigateTo} />}
     {currentPage === 'contact' && <ContactUs navigateTo={navigateTo} />}
     {currentPage === 'about' && <AboutUs navigateTo={navigateTo} />}
