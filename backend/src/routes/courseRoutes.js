50acb828a184d2bd1b9cf5f6fe981ee867794de3
Tharusha Dineth <tdineth53@gmail.com>
2025-09-29 23:22:51 +0530

    Update courseRoutes.js

--- backend/src/routes/courseRoutes.js
+++ backend/src/routes/courseRoutes.js
@@ -1,5 +1,18 @@
 import express from "express";
-import {createLesson ,createModule ,createCourse, getAllCourses, deleteCourse, getCourse, updateCourse, togglePublishCourse, getPendingCourses, approveCourse, rejectCourse } from "../controllers/courseController.js";
+import {
+    createLesson,
+    createModule,
+    createCourse,
+    getAllCourses,
+    deleteCourse,
+    getCourse,
+    updateCourse,
+    togglePublishCourse,
+    getPendingCourses,
+    approveCourse,
+    rejectCourse,
+    getInstructorCourses
+} from "../controllers/courseController.js";
 import {roleCheck} from "../middleware/roleCheck.js";
 import {authMiddleware} from "../middleware/authMiddleware.js";
 
@@ -18,11 +31,14 @@ courseRouter.post('/:id/module',roleCheck("INSTRUCTOR", "ADMIN") , createModule)
 courseRouter.post('/module/:moduleId/lesson',roleCheck("INSTRUCTOR", "ADMIN") , createLesson);
 courseRouter.put('/:id',roleCheck("INSTRUCTOR", "ADMIN"), updateCourse);
 courseRouter.patch('/:id/publish', roleCheck("INSTRUCTOR", "ADMIN") ,togglePublishCourse);
+courseRouter.get('/instructor/courses', roleCheck("INSTRUCTOR") , getInstructorCourses);
+
 
 // Admin only routes
 courseRouter.get('/admin/pending', roleCheck("ADMIN"), getPendingCourses);
 courseRouter.patch('/:id/approve', roleCheck("ADMIN"), approveCourse);
 courseRouter.patch('/:id/reject', roleCheck("ADMIN"), rejectCourse);
+
 
 export default courseRouter;
 
